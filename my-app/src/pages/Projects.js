d7e1024f80a8cc6a23967303c0b2c3f3577ec69c
radrake <radrake@uark.edu>
2022-09-24 21:50:24 -0500

    Added some filler text

--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -11,7 +11,12 @@ export default function Projects() {
           These are projects that I personally created without a tutorial. <br/>
           I showcase the kinds of projects I am interested in as well as simple resume projects.
         </p>
+        <p className="project-head">Insert project here!</p>
+        <p>
+          A project will go here once I decide to add one here!
+        </p>
       </body>
+
 
       <h2>School Projects</h2>
       <body>
@@ -19,7 +24,12 @@ export default function Projects() {
           These are projects that I worked on for school. <br/>
           I showcase the projects that were loosely defined, where most design choices were in our hands. <br/>
         </p>
+        <p className="project-head">Insert project here!</p>
+        <p>
+          A project will go here once I decide to add one here!
+        </p>
       </body>
+
 
       <h2>Tutorial Projects</h2>
       <body>
@@ -27,14 +37,15 @@ export default function Projects() {
           These are projects that I created by following a tutorial online. <br/>
           I showcase these so that you can see what kind of things I have been trying to learn.
         </p>
+
         <p className="project-head"><Link to="/pokemon">PokeAPI - Pokedex</Link></p>
-        
         <p>
           In this project, I learned how to fetch APIs and got more practice using React hooks. <br/>
           It is a very simple project which lets you scroll through a list of pokemon. <br/>
           I followed this Web Dev Simplified tutorial: <br/>
           <a href="https://www.youtube.com/watch?v=o3ZUc7zH8BE" target="_blank" rel="noreferrer">Build A Pokémon Application With React - Tutorial</a>
         </p>
+
         <p className="project-head">Navbar (The one used for this website!)</p>
         <p>
           In this project, I learned how to add a navbar to a website and route between pages seemlessly using React Router. <br/>
