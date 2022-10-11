74b98d79f90eec9c06b56cd843a980de95ac0648
radrake <98198543+radrake@users.noreply.github.com>
2022-10-11 10:18:31 -0500

    Update MyRouter.js
    Readded page not found page oops

--- my-app/src/MyRouter.js
+++ my-app/src/MyRouter.js
@@ -4,6 +4,7 @@ import Resume from './pages/Resume';
 import Projects from './pages/Projects';
 import Pokemon from './pages/projects/Pokemon';
 import About from './pages/About';
+import Error404 from './pages/Error404'
 import { Route, Routes } from "react-router-dom"
 
 export default function MyRouter() {
@@ -11,6 +12,7 @@ export default function MyRouter() {
     <>
         <Routes>
           <Route path="/" element={ <Home /> } />
+          <Route path="/*" element={ <Error404 />} />
           <Route path="/resume" element={ <Resume /> } />
           <Route path="/projects" element={ <Projects /> } />
           <Route path="/about" element={ <About /> } />
